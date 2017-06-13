e0643b442db3cfe560d658e191246bcb7594ed68
Ben Copeland <Ben.Copeland@milliman.com>
2017-06-13 15:58:55 -0400

    Add a first draft of execution script
    This has the basic functions for executing the EAPG grouper in parallel

---
+++ python/eapg/execution.py
@@ -0,0 +1,168 @@
+"""
+### CODE OWNERS: Ben Copeland
+
+### OBJECTIVE:
+  Functions for executing the EAPG software
+
+### DEVELOPER NOTES:
+  <none>
+"""
+import logging
+import typing
+import subprocess
+import os
+import shutil
+from pathlib import Path
+from functools import partial
+
+import pyspark.sql
+import pyspark.sql.functions as spark_funcs
+import pyspark.sql.types as spark_types
+
+import eapg.shared
+from prm.spark.io_txt import encode_rows_to_strings, build_structtype_from_csv
+from prm.spark.app import SparkApp
+
+LOGGER = logging.getLogger(__name__)
+
+EAPG_VERSION = '17039'
+PATH_EAPG_GROUPER = Path(r'C:\Program Files\3mhis\v2017.1.2\cgs\cgs_console.exe')
+
+PATH_TEMPLATES = Path(os.environ['eapg_grouper_home']) / 'templates'
+PATH_INPUT_TEMPLATE = PATH_TEMPLATES / 'prm_eapgs_in.2017.1.2.dic'
+PATH_OUTPUT_TEMPLATE = PATH_TEMPLATES / 'prm_eapgs_out.2017.1.2.dic'
+
+# pylint: disable=no-member
+
+# =============================================================================
+# LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
+# =============================================================================
+
+
+def _run_eapg_grouper_on_partition(# pylint: disable=too-many-locals
+        id_partition: int,
+        iter_claims: "typing.Iterable[str]",
+        *,
+        path_workspace: Path,
+        path_logs_public: typing.Optional[Path]=None,
+        cleanup_claim_copies: typing.Optional[bool]=True,
+        **kwargs_eapg
+    ) -> None: # pragma: no cover
+    """Execute the EAPG software on a single partition"""
+
+    path_eapg_io = Path.cwd() / "eapg_grouper_io" / "part_{}".format(id_partition)
+    path_eapg_io.mkdir(
+        parents=True,
+        exist_ok=True,
+        )
+    path_input_file = path_eapg_io / 'eapg_in.csv'
+    path_output_file = path_workspace / 'eapgs_out_{}.csv'.format(id_partition)
+
+
+    with path_input_file.open('w') as fh_input:
+        for claim in iter_claims:
+            fh_input.write(claim + "\n")
+
+    options = {
+        'input': path_input_file.as_posix(),
+        'input_template': PATH_INPUT_TEMPLATE.as_posix(),
+        'upload': path_output_file.as_posix(),
+        'upload_template': PATH_OUTPUT_TEMPLATE.as_posix(),
+        'input_header': 'off',
+        'schedule': 'off',
+        'grouper': EAPG_VERSION,
+        'input_date_format': 'yyyy-MM-dd',
+        }
+    if path_logs_public:
+        options['error_log'] = path_logs_public / 'error_log_{}.txt'.format(id_partition)
+        options['edit_log'] = path_logs_public / 'edit_log_{}.txt'.format(id_partition)
+    options.update(**kwargs_eapg)
+
+    args = [str(PATH_EAPG_GROUPER)]
+    for key, val in options.items():
+        args.append('-' + key)
+        args.append(str(val))
+
+    subprocess.run(
+        args,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+        check=True,
+        )
+
+
+    yield path_output_file.name
+
+    if cleanup_claim_copies:
+        shutil.rmtree(str(path_eapg_io)) # Cleanup extra claim copies lying around
+
+
+def run_eapg_grouper(
+        sparkapp: SparkApp,
+        input_dataframes: "typing.Mapping[str, pyspark.sql.DataFrame]",
+        path_output: Path,
+        *,
+        path_network_io: typing.Optional[Path]=None,
+        path_logs_public: typing.Optional[Path]=None,
+        cleanup_claim_copies: typing.Optional[bool]=True,
+        output_struct: typing.Optional[spark_types.StructType]=None,
+        **kwargs_eapg
+    ) -> "typing.Mapping[str, pyspark.sql.DataFrame]":
+    """Execute the EAPG software"""
+
+    if path_network_io:
+        path_workspace = path_network_io
+    else:
+        path_workspace = path_output / '_temp_eapg_grouper'
+
+    path_workspace.mkdir(
+        exist_ok=True,
+        )
+
+
+    rdd_claims = input_dataframes['claims'].rdd.mapPartitions(
+        partial(
+            encode_rows_to_strings,
+            schema=input_dataframes['claims'].schema,
+            delimiter_csv=',',
+            )
+        )
+    rdd_results = rdd_claims.mapPartitionsWithIndex(
+        partial(
+            _run_eapg_grouper_on_partition,
+            path_workspace=path_workspace,
+            path_logs_public=path_logs_public,
+            cleanup_claim_copies=cleanup_claim_copies,
+            **kwargs_eapg
+            )
+        )
+    rdd_results.count() # Force a realization
+    if not output_struct:
+        output_struct = build_structtype_from_csv(
+            eapg.shared.PATH_SCHEMAS / 'eapgs_out.csv'
+            )
+
+    partitions_output = [
+        str(_path)
+        for _path in path_workspace.glob('eapgs_out_*.csv')
+        ]
+    df_eapg_output = sparkapp.session.read.csv(
+        partitions_output,
+        schema=output_struct,
+        header=False,
+        )
+    sparkapp.save_df(
+        df_eapg_output,
+        path_output / 'eapgs_out.parquet',
+        )
+
+    if cleanup_claim_copies:
+        shutil.rmtree(str(path_workspace))
+
+    return df_eapg_output
+
+
+
+
+if __name__ == 'main':
+    pass

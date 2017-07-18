73a4e31677e45bdb0e86d294b53ad8665c575660
Chas Busenburg <chas.busenburg@milliman.com>
2017-07-18 15:26:46 -0400

    Use setup_env.bat from within release.bat
    Remove an instance of duplication

--- promote_release.bat
+++ promote_release.bat
@@ -11,19 +11,7 @@ rem  *none*
 rem LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Setting up promotion environment for product component
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Running from %~f0
-
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Calling latest pipeline-components-env file
-call "S:\PRM\Pipeline_Components_Env\pipeline_components_env.bat"
-
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Redirecting component HOME to local copy
-set EAPG_GROUPER_HOME=%~dp0
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: EAPG_GROUPER_HOME is now %EAPG_GROUPER_HOME%
-
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Prepending local python library to PYTHONPATH
-set PYTHONPATH=%EAPG_GROUPER_HOME%\python;%PYTHONPATH%
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: PYTHONPATH is now %PYTHONPATH%
-
+call %~dp0setup_env.bat
 echo Finished setting up promotion environment for product component
 
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Calling promotion script
